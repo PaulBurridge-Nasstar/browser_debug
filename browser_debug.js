e2f4e2465dfe041b70e480ab5a7e2065cd7a0302
Paul Burridge <pburridge@smart421.com>
2015-03-09 15:22:23 +0000

    Initial commit

---
+++ browser_debug.js
@@ -0,0 +1,38 @@
+if (typeof console === "undefined") {
+  console = {};
+  console.log = function() {
+    return;
+  };
+}
+
+(function($) { // Wrapper for Drupal 7 - http://drupal.org/node/1003664
+
+  $(document).ready(function () {
+
+    if(Drupal.ajax) {
+      Drupal.ajax.prototype.commands.browserDebugAjaxComplete =  function(ajax, response, status) {
+        console.log('browserDebugAjaxComplete');
+        printDebug(response.arguments);
+      };      
+    }
+
+    printDebug(Drupal.settings.browserDebug);
+
+  });
+
+  function printDebug(data) {    
+    console.log('Session:');
+    console.log(data.session);
+    
+    if(data.log.length > 0) {
+      console.log('Log:');
+      for(var i = 0; i < data.log.length; i++) {
+        console.log(data.log[i]);
+      }      
+    }
+
+  }
+
+
+})(jQuery);
+
